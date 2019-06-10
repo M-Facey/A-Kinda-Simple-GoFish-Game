29c2d04b21852b6a74ce6400ebc2cccb9016164a
M-Facey <faceymaxal17@gmail.com>
2019-06-10 08:57:03 -0500

    fifth commit

--- src/a_kinda_simple_gofish_game/A_Kinda_Simple_GoFish_Game.java
+++ src/a_kinda_simple_gofish_game/A_Kinda_Simple_GoFish_Game.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package a_kinda_simple_gofish_game;
 
 import javafx.application.Application;
@@ -25,9 +20,6 @@ public class A_Kinda_Simple_GoFish_Game extends Application {
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
